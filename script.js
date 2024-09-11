00bca48036a4cbd0a4a8e0052a06cfbae69ce69d
Faelrocks <120528074+Faelrocks@users.noreply.github.com>
2024-09-11 13:42:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const idades = [5, 12, 15, 17, 18, 19, 25, 32, 33, 54];
-const permitidos = idades.filter(habilitacao);
-
-function habilitacao(idades) {
-    return idades >= 18;
- }
-
-// console.log(permitidos)
